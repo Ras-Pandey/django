e4e8c6ef6c8951e42e8b23fccba92813e0185f3a
Ras-Pandey <149924456+Ras-Pandey@users.noreply.github.com>
2025-05-26 11:26:40 +0530

    hello

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include,path
+from users import views as user_views
+
+
 
 urlpatterns = [
     path('food/', include('food.urls')),
     path('admin/', admin.site.urls),
+    path('register/', user_views.register, name='register'),
 ]
